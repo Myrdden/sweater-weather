0dabd263fb9b2c5e212ec236ea1869bc749cead2
Aurie <Myrdden42@gmail.com>
2019-09-18 11:00:56 -0600

    Increased Legibility

--- app/base_controller.rb
+++ app/base_controller.rb
@@ -1,8 +1,9 @@
 class BaseController < Sinatra::Base
   get '/api/v1/forecast' do
     if params[:location]
-      coords = GoogleService.geocode(params[:location])
+      coords   = GoogleService.geocode(params[:location])
       forecast = DarkskyService.get_forecast(coords)
+
       json BaseSerializer.darksky(forecast)
     else
       status 400
@@ -21,8 +22,9 @@ class BaseController < Sinatra::Base
     body = JSON.parse(request.body.read).symbolize_keys
     if body[:api_key] && User.validate(body[:api_key])
       travel_time = GoogleService.travel_time(body)
-      coords = GoogleService.geocode(body[:destination])
-      forecast = DarkskyService.get_forecast_time(coords, (Time.now.to_i + travel_time))
+      coords      = GoogleService.geocode(body[:destination])
+      forecast    = DarkskyService.get_forecast_time(coords, (Time.now.to_i + travel_time))
+
       json BaseSerializer.darksky(forecast)
     else
       status 403
--- app/users/users_controller.rb
+++ app/users/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < Sinatra::Base
   post '/api/v1/users' do
     strong_params = ['email', 'password', 'password_confirmation']
-    body = JSON.parse(request.body.read).symbolize_keys
-    user_params = body.keep_if {|k,v| strong_params.include?(k)}
-    user = User.new(user_params)
+    body          = JSON.parse(request.body.read).symbolize_keys
+    user_params   = body.keep_if {|k,v| strong_params.include?(k)}
+    user          = User.new(user_params)
+
     if user.save
       json user.api_key
     else
@@ -13,9 +14,10 @@ class UsersController < Sinatra::Base
 
   post '/api/v1/sessions' do
     strong_params = ['email', 'password']
-    body = JSON.parse(request.body.read).symbolize_keys
-    user_params = body.keep_if {|k,v| strong_params.include?(k)}
-    user = User.find_by(email: user_params[:email])
+    body          = JSON.parse(request.body.read).symbolize_keys
+    user_params   = body.keep_if {|k,v| strong_params.include?(k)}
+    user          = User.find_by(email: user_params[:email])
+
     if user && user.authenticate(user_params[:password])
       json user.api_key
     else
