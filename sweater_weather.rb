ba628decf1223dfffa1fe595745213132163eb2f
Aurie <Myrdden42@gmail.com>
2019-09-18 11:15:20 -0600

    Setup Config Vars for Heroku

--- sweater_weather.rb
+++ sweater_weather.rb
@@ -1,15 +1,16 @@
 ENV['SINATRA_ACTIVESUPPORT_WARNING'] = 'false'
 ENV['RACK_ENV'] ||= 'development'
-ENV['SINATRA_ENV'] ||= 'development'
 
-require './secrets.rb'
-SECRETS.each do |k,v|
-  ENV[k] = v
+if ENV['RACK_ENV'] == 'development'
+  require './secrets.rb'
+  SECRETS.each do |k,v|
+    ENV[k] ||= v
+  end
 end
 
 require 'bundler/setup'
 require 'sinatra/base'
 require 'sinatra/activerecord'
-Bundler.require :default, ENV['SINATRA_ENV']
+Bundler.require :default, ENV['RACK_ENV']
 
 require_all 'app'
